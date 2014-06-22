cb4232dca92c94f94d0e8445e87c9c0a3424d25f
Faizan Sohail <fsohail86@gmail.com>
2014-06-22 23:13:29 +0100

    Added JSR303 validaton annotations to domain object variables

--- src/main/java/com/matrimonial/domain/User.java
+++ src/main/java/com/matrimonial/domain/User.java
@@ -4,56 +4,75 @@ import javax.persistence.Entity;
 import javax.persistence.Column;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import org.hibernate.annotations.Type;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.joda.time.LocalDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.matrimonial.validator.NotDefaultSelect;
 
 @Entity
 @Table(name = "matrimonial.users")
 public class User {
-
 	@Id
 	@Column(name = "username")
+	@NotEmpty
+	@Pattern(regexp = "^[a-zA-Z0-9]*$")
 	private String username;
 
 	@Column(name = "password", nullable = false)
+	@NotEmpty
+	@Size(min = 8)
 	private String password;
 
 	@Column(name = "first_name", nullable = false)
+	@NotEmpty
 	private String firstName;
 
 	@Column(name = "last_name", nullable = false)
+	@NotEmpty
 	private String lastName;
 
 	@Column(name = "email", nullable = false)
+	@NotEmpty
+	@Email
 	private String email;
 
 	@Column(name = "gender", nullable = false)
+	@NotEmpty
 	private String gender;
 
-	@Column(name = "profile_for", nullable = false)
-	private String profileFor;
-
 	@Column(name = "date_of_birth", nullable = false)
-	@Type(type="org.jadira.usertype.dateandtime.joda.PersistentLocalDate")
+	@Type(type = "org.jadira.usertype.dateandtime.joda.PersistentLocalDate")
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dateOfBirth;
 
+	@Column(name = "join_date", nullable = false)
+	@Type(type = "org.jadira.usertype.dateandtime.joda.PersistentLocalDate")
+	private LocalDate joinDate;
+
+	@Column(name = "profile_for", nullable = false)
+	@NotDefaultSelect
+	private String profileFor;
+
 	@Column(name = "ethnicity", nullable = false)
+	@NotDefaultSelect
 	private String ethnicity;
 
 	@Column(name = "religious_sect", nullable = false)
+	@NotDefaultSelect
 	private String religiousSect;
 
 	@Column(name = "community_language", nullable = false)
+	@NotDefaultSelect
 	private String communityLanguage;
-	
-	@Column(name = "join_date", nullable = false)
-	@Type(type="org.jadira.usertype.dateandtime.joda.PersistentLocalDate")
-	private LocalDate joinDate;
-	
-	@Column(name = "role", nullable = false)
-	private String role;
-	
+
 	@Column(name = "where_did_you_hear_about_us", nullable = false)
+	@NotDefaultSelect
 	private String whereDidYouHearAboutUs;
 
 	public String getUsername() {
@@ -104,20 +123,28 @@ public class User {
 		this.gender = gender;
 	}
 
-	public String getProfileFor() {
-		return profileFor;
-	}
-
-	public void setProfileFor(String profileFor) {
-		this.profileFor = profileFor;
-	}
-
 	public LocalDate getDateOfBirth() {
 		return dateOfBirth;
 	}
 
 	public void setDateOfBirth(LocalDate dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
+	}
+
+	public LocalDate getJoinDate() {
+		return joinDate;
+	}
+
+	public void setJoinDate(LocalDate joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	public String getProfileFor() {
+		return profileFor;
+	}
+
+	public void setProfileFor(String profileFor) {
+		this.profileFor = profileFor;
 	}
 
 	public String getEthnicity() {
@@ -143,23 +170,7 @@ public class User {
 	public void setCommunityLanguage(String communityLanguage) {
 		this.communityLanguage = communityLanguage;
 	}
-	
-	public LocalDate getJoinDate() {
-		return joinDate;
-	}
 
-	public void setJoinDate(LocalDate joinDate) {
-		this.joinDate = joinDate;
-	}
-	
-	public String getRole() {
-		return role;
-	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
-	
 	public String getWhereDidYouHearAboutUs() {
 		return whereDidYouHearAboutUs;
 	}
